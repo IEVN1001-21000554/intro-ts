23f22f9aff2b57ab4d48e2a9e51e7ce1db1cebe3
Miguel Rivera <riveragarcia.miguelangel.00e3@gmail.com>
2024-09-10 20:59:43 -0600

    segundo commit del martes 10

---
+++ src/temas-ts/06-exportar-importar.ts
@@ -0,0 +1,18 @@
+import {calcularIVA2, Producto} from "./05-desestructurar-funciones.ts"
+
+const productosN: Producto[] = [
+    {
+        desc: "Telefono1",
+        precio: 1000
+    },
+    {
+        desc: "Telefono2",
+        precio: 2000
+    },
+    {
+        desc: "Telefono3",
+        precio: 3000
+    }
+]
+
+
